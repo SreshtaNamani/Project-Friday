7c7c80a9becbc7085fc2a3d05836a8e3ac1fba0a
SreshtaNamani <sreshtagopinadh@gmail.com>
2026-07-23 23:15:21 +0530

    assistant.py Phase - 1 code added

---
+++ assistant.py
@@ -0,0 +1,30 @@
+def greet():
+    print("Friday: Hello!")
+
+def show_help():
+    print('''
+    Available commands:
+    - Hello
+    - Help
+    - Exit
+    ''')
+
+    COMMANDS = {"hello" : greet,
+                "help" : show_help,
+                }
+
+def start_assistant():
+    print("-" * 20)
+    print("      Welcome to Friday")
+    print("-" * 20)
+
+    while True:
+        command = input("\nYou: ").strip().lower()
+        if command == "exit":
+            print("Friday: Goodnight")
+            break
+        action COMMANDS.get(command)
+        if action: 
+            action()
+        else:
+            print("Friday: Sorry, I don't understand that.")
